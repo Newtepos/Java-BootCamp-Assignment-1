f2bbb2e9e8e649b7680562fd779edd250ae9fc9c
Thipsukhon <thipsukhon@Thipsukhons-MacBook-Pro.local>
2022-02-26 20:16:53 +0700

    #5: Login api complete

--- src/main/java/com/example/shoppingapp/user/UserController.java
+++ src/main/java/com/example/shoppingapp/user/UserController.java
@@ -1,7 +1,21 @@
 package com.example.shoppingapp.user;
 
+import com.example.shoppingapp.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/login")
+    public UserResponse login(@RequestBody User user) {
+        String userLogin = userService.login(user);
+        return new UserResponse(userLogin);
+    }
 }
--- src/main/java/com/example/shoppingapp/user/UserService.java
+++ src/main/java/com/example/shoppingapp/user/UserService.java
@@ -5,6 +5,8 @@ import com.example.shoppingapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -13,6 +15,17 @@ public class UserService {
 
     public void createUser(User user) {
         userRepository.save(user);
+    }
+
+    public String login(User user) {
+        Optional<User> QueryUser = userRepository.findByUsername(user.getUsername());
+        if(QueryUser.isPresent()) {
+          if(QueryUser.get().getPassword().equals(user.getPassword())) {
+              return "login success";
+          }
+              return  "password incorrect";
+        }
+        return "User not found";
     }
 
 }
