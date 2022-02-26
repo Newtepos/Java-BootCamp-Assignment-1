795f51a13c359089edb233607060014f40e1ded0
Thipsukhon <thipsukhon@Thipsukhons-MacBook-Pro.local>
2022-02-26 18:09:07 +0700

    #2: create user

--- src/main/java/com/example/shoppingapp/entities/User.java
+++ src/main/java/com/example/shoppingapp/entities/User.java
@@ -1,2 +1,19 @@
-package com.example.shoppingapp.entities;public class User {
+package com.example.shoppingapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+    public String username;
+    public String password;
+
+
 }
