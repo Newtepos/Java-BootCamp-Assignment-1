5af74d74bf527aa5cf98a292be0d6e037f63b22d
Thipsukhon <thipsukhon@Thipsukhons-MacBook-Pro.local>
2022-02-26 18:29:22 +0700

    #2: create product

---
+++ src/main/java/com/example/shoppingapp/entities/Product.java
@@ -0,0 +1,49 @@
+package com.example.shoppingapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+    public String productName;
+    public double price;
+
+    public Product() {
+    }
+
+    public Product(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
